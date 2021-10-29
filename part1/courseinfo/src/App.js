8c41a962fb16c7687330a3524409d1263e15b3bc
Gerald <geraldmir4@gmail.com>
2021-10-29 10:33:28 +0800

    Exercise 1.1-1.2 done

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part part={part1} exercise={exercises1} />
+        <Part part={part2} exercise={exercises2} />
+        <Part part={part3} exercise={exercises3} />
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
