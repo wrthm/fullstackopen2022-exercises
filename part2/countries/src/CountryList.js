de92a0041e1237d1b4fb7b3571f0213ac0927c0b
Gerald <geraldmir4@gmail.com>
2022-06-17 18:07:02 +0800

    Refactor code to use fewer lines

--- part2/countries/src/CountryList.js
+++ part2/countries/src/CountryList.js
@@ -1,9 +1,7 @@
 const CountryList = ({ countries, setFilter }) => {
-  const showCountryCallback = country => () => setFilter(country)
-
   return countries.map(country => (
     <div key={country.cca2}>
-      {country.name.common} <button onClick={showCountryCallback(country.name.common)}>Show</button>
+      {country.name.common} <button onClick={() => setFilter(country.name.common)}>Show</button>
     </div>
   ))
 }
