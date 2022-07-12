a6fba31925f65207f7e7ed6cebe9fa66f8999878
Gerald <geraldmir4@gmail.com>
2022-07-12 11:28:49 +0800

    Exercise 4.4 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, value) => sum + value.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
